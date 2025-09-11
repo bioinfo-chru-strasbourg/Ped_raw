f29a6a75ef67e714738dd5965400d3cae281e845
SamuelNicaise <ichtyornis@gmail.com>
2025-09-11 18:21:10 +0200

    Use router instead of href, make navbar look more like hviewer

--- Front/src/App.vue
+++ Front/src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <ul class="list-unstyled">
-    <div class="nav-scroller py-1 mb-2">
-      <nav class="nav d-flex justify-content-between">
-        <div style="display: inline-flex; float: left">
-          <a class="text-dark" href="/">
-            <img src="/logoprov5.png" alt="logo"></a>
-          <h1><a class="text-dark" href="/" style="serif;
-                  font-size: 4rem;  text-decoration: none; margin: 2.5rem;">
-              Ped_raw</a>
-          </h1>
-        </div>
-        <div style="display: inline-flex; float: right; margin-top: -1.5rem">
-          <Button class="mb-2" severity="secondary" style="font-size: 1.9rem;  margin-top: 0.5rem;" text> <a href="/"
-              class="navbar-brand"> Pedigree </a> </Button>
-          <Button class="mb-2" severity="secondary" style="font-size: 1.85rem; margin: 0.5rem;" text>
-            <a href="/doc" class="navbar-brand"> Documentation </a>
-          </Button>
-        </div>
-      </nav>
-      <hr style=" margin-left: 10rem; margin-top: -1.5rem; margin-bottom: 2rem;">
-    </div>
-
-  </ul>
-
+  <div class="nav-scroller py-1">
+    <router-link to="/" class="navbar-brand">
+      <img src="/logoprov5.png" alt="logo" style="float:left">
+    </router-link>
+      <div class="d-flex justify-content-between align-items-center" style="">
+          <div style="">
+              <router-link to="/" class="navbar-brand">
+                  <Button class="mb-2" severity="secondary" style="font-size: 3.5rem; color: black; margin-left:1.5rem ;padding-top:0; padding-bottom:0"
+                      text>Ped_raw
+                  </Button>
+              </router-link>
+          </div>
+          <div style="">
+              <router-link to="/" class="navbar-brand">
+                  <Button class="mb-2 navbutton" severity="secondary" text>Pedigree</Button>
+              </router-link>
+              <router-link to="/doc" class="navbar-brand">
+                  <Button class="mb-2 navbutton" severity="secondary" text>About</Button>
+              </router-link>
+              <a href="mailto:bioinfo@chru-strasbourg.fr" class="navbar-brand">
+                  <Button class="mb-2 navbutton" severity="secondary" text>Contact us</Button>
+              </a>
+          </div>
+      </div>
+      <hr style=" margin-left: 10rem; margin-top: 0.5rem; margin-bottom: 2rem;">
+  </div>
   <RouterView />
 </template>
 
@@ -35,13 +37,15 @@ import { RouterView } from 'vue-router'
 
 
 #app {
-  margin-top: 1rem !important;
+  /* margin-top: 1rem !important; */
   margin-left: 0.5rem !important;
   margin-right: 0.5rem !important;
   padding: 0.5rem !important;
 }
 
-a {
-  font-family: 'Playfair Display', serif;
+.navbutton {
+  /* font-family: 'Playfair Display', serif !important; */
+  color: #495978 !important;
+  font-size:2rem !important;
 }
 </style>
